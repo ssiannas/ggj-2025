2114e37f12dbda8a9052904f138d8030ef71e9ee
ssiannas <ssiannas@gmail.com>
2025-01-25 00:19:32 +0200

    Improve movement

--- Assets/Scripts/PlayerMovementController.cs
+++ Assets/Scripts/PlayerMovementController.cs
@@ -9,11 +9,10 @@ public class PlayerMovementController : MonoBehaviour
     {
         // Get values from unity input systwm
         var input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
-        
+        input = input.normalized;
+        input *= speed * Time.deltaTime;
         // move left - right by horizontal input and up, down by vertical input (normalize for when you press two at the same time)
-        var factorX = speed * input.x * Time.deltaTime;
-        var factorY = speed * input.y * Time.deltaTime;
-        transform.Translate(Vector3.up * factorY, Space.Self);
-        transform.Translate(Vector3.right * factorX, Space.Self);
+        
+        transform.Translate(new Vector3(input.x, input.y, 0));
     }
 }
